205548b3b0d7eec03ec65e91bab1cc4e2aa13c66
ishaanjindal15 <67318299+ishaanjindal15@users.noreply.github.com>
2021-03-29 13:40:10 +0530

    Update tree.js

--- tree.js
+++ tree.js
@@ -1,17 +1,23 @@
-class Tree {
+class Stone {
     constructor(x,y,width,height) {
       var options = {
-          isStatic: true
+          isStatic: false,
+          resitution:0,
+          fricton:1,
+          density:1.2
       }
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
       World.add(world, this.body);
-      this.image=loadImage("images/tree.png")    }
+      this.image= loadImage("images/stone.png")
+    }
     display(){
       var pos =this.body.position;
-      rectMode(CENTER);
-      fill("brown");
-      rect(pos.x, pos.y, this.width, this.height);
+    //  rectMode(CENTER);
+      //fill("brown");
+      //rect(pos.x, pos.y, this.width, this.height);
+      imageMode(CENTER)
+      image(this.image,pos.x, pos.y, this.width, this.height)
     }
   };
